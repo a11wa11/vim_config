8b2d30313a8a7b97c3a24c6ba5297ece6cc587a0
awa <kenshin561183@gmail.com>
2020-10-20 21:29:22 +0900

    add git comments

--- setting.sh
+++ setting.sh
@@ -5,3 +5,10 @@ ln -nsf $(pwd)/template_bashrc.txt ~/.bashrc
 ln -nsf $(pwd)/vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# git config --global user.name "First-name Family-name"
+# git config --global user.email "username@example.com"
+# git config --global color.diff auto
+# git config --global color.status auto
+# git config --global color.branch auto
+# git config --global tool vimdiff
+# git config --global prompt false
