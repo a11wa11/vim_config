f04c537990ba07acf5e4d9d4e0c8b88a1aa2b2ef
awa <kenshin561183@gmail.com>
2021-07-17 22:53:24 +0900

    fixed

--- initial.sh
+++ initial.sh
@@ -6,8 +6,10 @@
 # M1チップの場合は、「Rossetaを使用して開く」をターミナルに設定してから実行しないとHOME BREWはインストールできない
 [ ! -e /usr/local/bin/brew ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # gitの初期設定
-git_name=`git config --get user.name`
-git_email=`git config --get user.email`
+check_git_info () {
+    git_name=`git config --get user.name`
+    git_email=`git config --get user.email`
+}
 
 register_git_info () {
     target=$1
@@ -18,6 +20,8 @@ register_git_info () {
     git config --global user.${target} ${git_var}
     echo "   gitの登録${target_jp}は -> ${git_var} です"
 }
+
+check_git_info
 
 [ ! ${git_name} ] && register_git_info "name" "名"
 [ ! ${git_email} ] && register_git_info "email" "メール"
@@ -32,12 +36,16 @@ fi
 # 初期設定するリポジトリをクローン
 [ ! -e ~/my-config ] && git clone https://github.com/a11wa11/my-config.git ~/my-config
 [ ! -e ~/.*shrc ] && sh ~/my-config/setting.sh
+check_git_info
+cp -f ~/my-config/git/gitconfig ~/.gitconfig
+git config --global user.name ${git_name}
+git config --global user.email ${git_email}
 
 #sudo sed -ie 's/^#log_path = \var\/log\/ansible.log/log_path = \var\/tmp\/ansible.log/g' /etc/ansible/ansible.cfg
 #sudo sed -ie 's/^#stdout_callback = skippy/#stdout_callback = skippy\nstdout_callback = counter_enabled/g' /etc/ansible/ansible.cfg
-brew install anyenv jq tree ansible@2.9
-brew install google-chrome brave-browser firefox
-brew install docker zoom discord
-brew install iterm2 cheatsheet
-brew install google-japanese-ime hyperswitch itsycal
-brew install mi visual-studio-code
+#brew install anyenv jq tree ansible@2.9
+#brew install google-chrome brave-browser firefox
+#brew install docker zoom discord
+#brew install iterm2 cheatsheet
+#brew install google-japanese-ime hyperswitch itsycal
+#brew install mi visual-studio-code
